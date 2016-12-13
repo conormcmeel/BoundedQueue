eab6921c1c709382c123505dcc54231abd41062f
Conrad Special <conormcmeel@gmail.com>
2016-12-13 14:49:02 +0000

    Renamed test

--- src/test/java/com/bounded/queue/BoundedQueueTest.java
+++ src/test/java/com/bounded/queue/BoundedQueueTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ThreadSafeBoundedQueueTest {
+public class BoundedQueueTest {
 
     @Test
     public void oneObjectShouldRemainUnconsumed() throws InterruptedException {
