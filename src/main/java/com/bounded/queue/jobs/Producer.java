41cc4ef7fbd995250c3ddaac5061560091196349
Conrad Special <conormcmeel@gmail.com>
2017-04-30 13:30:28 +0100

    new commie

--- src/main/java/com/bounded/queue/jobs/Producer.java
+++ src/main/java/com/bounded/queue/jobs/Producer.java
@@ -3,9 +3,11 @@ package com.bounded.queue.jobs;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static java.util.logging.Logger.*;
+
 public abstract class Producer implements Runnable {
 
-    private final static Logger logger = Logger.getLogger(Producer.class.getName());
+    private final static Logger logger = getLogger(Producer.class.getName());
 
     protected void logException(InterruptedException ex) {
         logger.log(Level.SEVERE, Thread.currentThread().getName() + "interrupted", ex);
